9612530037f52cfecb955f33e55035bc5b580b6e
seokjin8678 <seokjin8678@gmail.com>
2024-01-17 20:51:15 +0900

    refactor: TODO 제거, 제네릭 명시하여 return type 제거

--- src/api/admin/AdminSchoolService.ts
+++ src/api/admin/AdminSchoolService.ts
@@ -1,7 +1,6 @@
 import { PagingRequest } from '@/api/PagingRequest.ts';
 import { SearchRequest } from '@/api/SearchRequest.ts';
 import ApiService from '@/api';
-import { AxiosResponse } from 'axios';
 import UpdateSchoolApiSpec, {
   UpdateSchoolRequest,
   UpdateSchoolResponse,
@@ -14,28 +13,12 @@ import CreateSchoolApiSpec, {
 import DeleteSchoolApiSpec, { DeleteSchoolResponse } from '@/api/spec/school/DeleteSchoolApiSpec.ts';
 import FetchOneSchoolApiSpec, { FetchOneSchoolResponse } from '@/api/spec/school/FetchOneSchoolApiSpec.ts';
 
-/**
- * TODO 미래에 다음과 같은 형식이 되어야 함
- * ApiResponse 참조
- * {
- *   errorCode: null
- *   message: null,
- *   result: [
- *     {
- *       id: 1,
- *       domain: teco.ac.kr,
- *       name: 테코대학교
- *     }
- *   ]
- * }
- */
-
 const AdminSchoolService = {
-  fetchOneSchool(schoolId: number): Promise<AxiosResponse<FetchOneSchoolResponse>> {
-    return ApiService.request(FetchOneSchoolApiSpec(schoolId));
+  fetchOneSchool(schoolId: number) {
+    return ApiService.request<FetchOneSchoolResponse>(FetchOneSchoolApiSpec(schoolId));
   },
-  fetchSchools(paging: PagingRequest, search: SearchRequest): Promise<AxiosResponse<FetchSchoolsResponse>> {
-    return ApiService.request(FetchSchoolsApiSpec, {
+  fetchSchools(paging: PagingRequest, search: SearchRequest) {
+    return ApiService.request<FetchSchoolsResponse>(FetchSchoolsApiSpec, {
       page: paging.page,
       size: paging.itemsPerPage,
       sortBy: paging.sortBy[0]?.key,
@@ -44,14 +27,14 @@ const AdminSchoolService = {
       filterKeyword: search.filterKeyword,
     });
   },
-  createSchool(request: CreateSchoolRequest): Promise<AxiosResponse<CreateSchoolResponse>> {
-    return ApiService.request(CreateSchoolApiSpec, request);
+  createSchool(request: CreateSchoolRequest) {
+    return ApiService.request<CreateSchoolResponse>(CreateSchoolApiSpec, request);
   },
-  updateSchool(schoolId: number, request: UpdateSchoolRequest): Promise<AxiosResponse<UpdateSchoolResponse>> {
-    return ApiService.request(UpdateSchoolApiSpec(schoolId), request);
+  updateSchool(schoolId: number, request: UpdateSchoolRequest) {
+    return ApiService.request<UpdateSchoolResponse>(UpdateSchoolApiSpec(schoolId), request);
   },
-  deleteSchool(schoolId: number): Promise<AxiosResponse<DeleteSchoolResponse>> {
-    return ApiService.request(DeleteSchoolApiSpec(schoolId));
+  deleteSchool(schoolId: number) {
+    return ApiService.request<DeleteSchoolResponse>(DeleteSchoolApiSpec(schoolId));
   },
 };
 
