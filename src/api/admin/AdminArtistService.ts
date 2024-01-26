32df697bcf11d38e5a53445ac80115ea3ade9b33
seokjin8678 <seokjin8678@gmail.com>
2024-01-27 02:31:28 +0900

    feat: CreateArtistApiSpec, createArtist API 추가

--- src/api/admin/AdminArtistService.ts
+++ src/api/admin/AdminArtistService.ts
@@ -1,9 +1,16 @@
 import ApiService from '@/api';
-import FetchArtistsApiSpec, { FetchArtistsResponse } from '@/api/spec/artist/FetchArtists.ts';
+import FetchArtistsApiSpec, { FetchArtistsResponse } from '@/api/spec/artist/FetchArtistsApiSpec.ts';
+import CreateArtistApiSpec, {
+  CreateArtistRequest,
+  CreateArtistResponse,
+} from '@/api/spec/artist/CreateArtistApiSpec.ts';
 
 const AdminArtistService = {
   fetchArtists() {
     return ApiService.request<FetchArtistsResponse>(FetchArtistsApiSpec);
+  },
+  createArtist(request: CreateArtistRequest) {
+    return ApiService.request<CreateArtistResponse>(CreateArtistApiSpec, request);
   },
 };
 
