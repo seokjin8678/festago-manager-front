2ac00a6aab9f80360ae5d0d14747d3ec7fb56a82
seokjin8678 <seokjin8678@gmail.com>
2024-02-22 04:31:57 +0900

    feat: 관리자 축제 생성 페이지 추가

--- src/router/RouterPath.ts
+++ src/router/RouterPath.ts
@@ -4,13 +4,13 @@ import RootAdminView from '@/views/root/RootAdminView.vue';
 import AdminMyPageView from '@/views/admin/AdminMyPageView.vue';
 import AdminSchoolManageListView from '@/views/admin/school/AdminSchoolManageListView.vue';
 import NotFoundView from '@/views/common/NotFoundView.vue';
-import AdminFestivalManageView from '@/views/admin/AdminFestivalManageView.vue';
 import AdminSchoolManageCreateView from '@/views/admin/school/AdminSchoolManageCreateView.vue';
 import AdminSchoolManageEditView from '@/views/admin/school/AdminSchoolManageEditView.vue';
 import SchoolFestivalManageView from '@/views/school/SchoolFestivalManageView.vue';
 import AdminArtistManageListView from '@/views/admin/artist/AdminArtistManageListView.vue';
 import AdminArtistManageCreateView from '@/views/admin/artist/AdminArtistManageCreateView.vue';
 import AdminArtistManageEditView from '@/views/admin/artist/AdminArtistManageEditView.vue';
+import AdminFestivalManageCreateView from '@/views/admin/festival/AdminFestivalManageCreateView.vue';
 
 const RouterPath = {
   Common: {
@@ -58,11 +58,6 @@ const RouterPath = {
       name: 'adminSchoolManageEditPage',
       component: AdminSchoolManageEditView,
     },
-    AdminFestivalManagePage: {
-      path: '/admin/festivals',
-      name: 'adminFestivalManagePage',
-      component: AdminFestivalManageView,
-    },
     AdminArtistManageCreatePage: {
       path: '/admin/artist/create', // 명시적으로 단수형 사용
       name: 'adminArtistManageCreatePage',
@@ -77,6 +72,11 @@ const RouterPath = {
       path: '/admin/artist/edit/:id', // 명시적으로 단수형 사용
       name: 'adminArtistManageEditPage',
       component: AdminArtistManageEditView,
+    },
+    AdminFestivalManageCreatePage: {
+      path: '/admin/festival/create', // 명시적으로 단수형 사용
+      name: 'adminFestivalManageCreatePage',
+      component: AdminFestivalManageCreateView,
     },
   },
   School: {
--- src/views/admin/AdminFestivalManageView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <h1>관리자가 축제를 관리하는 페이지</h1>
-</template>
