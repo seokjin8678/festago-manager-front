1a25340df080fbfb9a3d186c22039b013ac17e3e
seokjin8678 <seokjin8678@gmail.com>
2024-04-01 22:32:44 +0900

    feat: Dockerfile 추가 및 관련 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+  },
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
