510859292eb02ea0aa1a7475d6f794b13de1db1b
seokjin8678 <seokjin8678@gmail.com>
2024-04-05 22:34:53 +0900

    feat: 아티스트 목록 페이징, 검색 기능 추가

--- src/views/admin/artist/AdminArtistManageListView.vue
+++ src/views/admin/artist/AdminArtistManageListView.vue
@@ -5,32 +5,48 @@ import DataTable from '@/components/datatable/DataTable.vue';
 import { Ref, ref } from 'vue';
 import AdminArtistService from '@/api/admin/AdminArtistService.ts';
 import { FetchArtistsResponse } from '@/api/spec/artist/FetchArtistsApiSpec.ts';
+import SearchForm from '@/components/form/SearchForm.vue';
+import { SearchRequest } from '@/api/SearchRequest.ts';
+import { PagingRequest } from '@/api/PagingRequest.ts';
 
 const tableHeaders = [
   { title: 'ID', key: 'id' },
   { title: '이름', key: 'name' },
-  { title: '프로필 URL', key: 'profileImage' },
   { title: '수정/삭제', key: 'actions', sortable: false },
 ];
-
+const searchFilters = [
+  { title: 'ID', value: 'id' },
+  { title: '이름', value: 'name' },
+];
 const itemsPerPageOptions = [
-  { value: 1, title: '1' },
   { value: 10, title: '10' },
   { value: 25, title: '25' },
   { value: 50, title: '50' },
 ];
 
-const items: Ref<FetchArtistsResponse> = ref([]);
 const loading = ref(false);
+const itemsPerPage = ref(10);
+const searchRequest: Ref<SearchRequest> = ref({ searchFilter: null, searchKeyword: null });
+const items: Ref<FetchArtistsResponse> = ref({ content: [], totalElements: 0 });
 
-function fetch() {
+function search(origin: SearchRequest) {
+  searchRequest.value = origin;
+  fetch({
+    page: 1,
+    itemsPerPage: itemsPerPage.value,
+    sortBy: [],
+  });
+}
+
+function fetch(paging: PagingRequest) {
   loading.value = true;
   setTimeout(() => (loading.value = false), 1000);
-  AdminArtistService.fetchArtists().then(response => {
+  AdminArtistService.fetchArtists(paging, searchRequest.value).then(response => {
     items.value = response.data;
     loading.value = false;
   });
 }
+
 </script>
 
 <template>
@@ -39,13 +55,17 @@ function fetch() {
     :flat="true"
     title="아티스트 목록"
   >
+    <SearchForm
+      :search="search"
+      :search-filters="searchFilters"
+    />
     <DataTable
       :loading="loading"
       :table-headers="tableHeaders"
       :items-per-page-options="itemsPerPageOptions"
       :fetch="fetch"
-      :item-length="items.length"
-      :items="items"
+      :item-length="items.totalElements"
+      :items="items.content"
       :detail-page-router-name="RouterPath.Admin.AdminArtistManageEditPage.name"
     />
   </v-card>
