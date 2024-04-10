db3bd341abcff67fc9edd6a8a4b0d60e05ae1bbc
seokjin8678 <seokjin8678@gmail.com>
2024-04-10 19:12:07 +0900

    feat: Readonly Form, Field 추가

---
+++ src/components/form/textfield/ReadonlyField.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const value = defineModel<string | number | undefined | null>('value', { required: true });
+const props = defineProps<{
+  label: string,
+}>();
+</script>
+
+<template>
+  <v-text-field
+    :label="props.label"
+    :model-value="value"
+    variant="outlined"
+    :readonly="true"
+  />
+</template>
