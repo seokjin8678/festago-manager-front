a60f0525860a3d346fb4c6cd9a3e8f79f70a4989
seokjin8678 <seokjin8678@gmail.com>
2024-04-10 20:13:00 +0900

    feat: SimpleTable 추가

---
+++ src/components/table/SimpleTable.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  title: string,
+  thead: string[]
+}>();
+
+</script>
+
+<!-- 다음과 같이 사용할 것
+<SimpleTable
+  title="공연 정보"
+  :thead="['시작시간', '티켓 오픈 시간', '아티스트 목록']"
+>
+  <tr v-for="stage in stages">
+    <td>{{ stringToDateString(stage.startDateTime) }}</td>
+    <td>{{ stringToDateString(stage.ticketOpenTime) }}</td>
+    <td>{{ stage.artists.map(artist => artist.name).join(', ') }}</td>
+  </tr>
+</SimpleTable>
+-->
+<template>
+  <h3 class="my-2 pt-5">{{ props.title }}</h3>
+  <v-table>
+    <thead>
+    <tr>
+      <th v-for="head in props.thead" :id="head">
+        {{ head }}
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <slot></slot>
+    </tbody>
+  </v-table>
+</template>
