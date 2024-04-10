116de067a1ce35f86704c2e2c9af5dd8381e868c
seokjin8678 <seokjin8678@gmail.com>
2024-04-10 20:58:13 +0900

    feat: ActionTab, ActionButton 추가

---
+++ src/components/tab/ActionButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  name: string,
+  icon: string
+}>();
+const emits = defineEmits<{
+  (e: 'action'): void
+}>();
+</script>
+
+<!-- 다음과 같이 사용할 것
+<ActionButton
+  name="공연 추가"
+  icon="mdi-plus-box-multiple-outline"
+  @action="$router.push(RouterPath.Admin.AdminStageManageCreatePage)"
+/>
+-->
+<template>
+  <v-col :cols="3">
+    <v-card
+      class="py-2"
+      variant="outlined"
+      @click="emits('action')"
+    >
+      <v-card-item>
+        <span v-if="props.icon" :class="`mdi ${props.icon}`" />
+        {{ props.name }}
+      </v-card-item>
+    </v-card>
+  </v-col>
+</template>
