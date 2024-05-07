86782f3ac0ec4930b8b45b86010d0f4238cd6bd4
seokjin8678 <seokjin8678@gmail.com>
2024-05-07 15:32:37 +0900

    fix: 개발 환경 표시 문자열 비교로 변경

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_IS_DEV: boolean;
+  readonly VITE_IS_DEV: string;
 }
 
 interface ImportMeta {
