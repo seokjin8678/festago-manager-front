d2a85c82660108f89996dbbad09a0bf93a3337c8
Seokjin Jeon <seokjin8678@gmail.com>
2024-05-07 14:58:44 +0900

    feat: 실행 환경이 개발 환경일 경우 상단에 표시 (#146) (#147)

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -2,8 +2,15 @@
 import { useDrawerStore } from '@/stores/useDrawerStore.ts';
 import AuthButton from '@/components/navigation/AuthButton.vue';
 import Drawer from '@/components/navigation/drawer/Drawer.vue';
+import { onMounted, ref } from 'vue';
 
 const drawerStore = useDrawerStore();
+const isDev = ref(false)
+
+onMounted(() => {
+  isDev.value = import.meta.env.DEV
+})
+
 </script>
 
 <template>
@@ -13,6 +20,9 @@ const drawerStore = useDrawerStore();
     <v-app-bar-title>
       <span class="text-body-1 text-md-h6">
         페스타고 관리자 페이지
+      </span>
+      <span class="text-body-1 text-md-h6 text-red-darken-4"  v-if="isDev">
+        (개발 서버)
       </span>
     </v-app-bar-title>
     <AuthButton />
